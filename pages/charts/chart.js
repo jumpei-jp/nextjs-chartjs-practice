9d1d01c33785da701a57e3081f6dd3ac32c3f334
jumpei-jp <junpeiyamaoka0516@gmail.com>
2022-06-01 14:13:16 +0900

    複数データをラインチャートに追加

--- pages/charts/chart.js
+++ pages/charts/chart.js
@@ -7,19 +7,47 @@ function Chart() {
   //userDataが変数でsetUserDataメソッドを使ってuserDataの値を変更
   const [userData, setUserData] = useState({
     labels: UserData.map((data) => data.year),
-    datasets: [{
-      label: "Users Gained",
-      data: UserData.map((data) => data.userGain),
-      backgroundColor: [
-      "rgba(75,192,192,1)",
-      "#ecf0f1",
-      "#50AF95",
-      "#f3ba2f",
-      "#2a71d0",
-    ],
-    borderColor: "black",
-    borderWidth: 2,
-    }]
+    datasets: [
+      {
+        label: "Users Gained",
+        data: UserData.map((data) => data.userGain),
+        backgroundColor: [
+        "rgba(75,192,192,1)",
+        "#ecf0f1",
+        "#50AF95",
+        "#f3ba2f",
+        "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+      {
+        label: "userLost",
+        data: UserData.map((data) => data.userLost),
+        backgroundColor: [
+        "rgba(75,192,192,1)",
+        "#ecf0f1",
+        "#50AF95",
+        "#f3ba2f",
+        "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      },
+      {
+        label: "Test",
+        data: UserData.map((data) => data.test),
+        backgroundColor: [
+        "rgba(75,192,192,1)",
+        "#ecf0f1",
+        "#50AF95",
+        "#f3ba2f",
+        "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      }
+    ]
   })
 
   return(
